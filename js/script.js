34cdcd02bcae373e5d7ea0e51faf32ee34ca041c
Kacper Stroisz <kacperstroisz@wp.pl>
2018-02-02 07:33:20 +0100

    Add stringi.html and .script

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var dinosaur = 'triceratops';
+var wielkieLitery = dinosaur.toUpperCase();
+console.log(wielkieLitery);
+
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var newText = text.replace('Velociraptor', 'Dinosaur');
+console.log(newText);
+
+console.log(text.length/2);
+console.log(text.substr(0,73));
