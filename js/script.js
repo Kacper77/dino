fdab4eac3d47731bc6979b934006e1ee29aa135e
Kacper Stroisz <kacperstroisz@wp.pl>
2018-02-02 18:45:20 +0100

    Very many change .js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 var dinosaur = 'triceratops';
-var wielkieLitery = dinosaur.toUpperCase();
-console.log(wielkieLitery);
+var nowyText = dinosaur.toUpperCase();
+console.log(nowyText);
 
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var newText = text.replace('Velociraptor', 'Dinosaur');
-console.log(newText);
+var nowyText = text.replace('Velociraptor', nowyText);
 
-console.log(text.length/2);
-console.log(text.substr(0,73));
+var nowy = nowyText.substr(0, nowyText.length/2);
+console.log(nowy);
